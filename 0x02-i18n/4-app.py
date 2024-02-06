67ce524fa82c96db952d5bb260be5f6239007d3a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-06 13:10:27 +0000

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,35 +2,37 @@
 """Flask app with forced locale support"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask_babel import Babel
 
 
 class Config:
-    """Config class"""
+    """Configuration class"""
+
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Loacle selector"""
-    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
-        return request.args['locale']
-
+def get_locale() -> str:
+    """Locale Selector"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Default route"""
-    return render_template('4-index.html', title=_('home_title'), header=_('home_header'))
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
