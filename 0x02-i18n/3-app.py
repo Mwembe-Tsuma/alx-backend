87c249ac9e9cc22d0d776fa06576ccadd256bdf4
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-06 12:05:50 +0000

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Basic Flask app with localization"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Selector"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Default route"""
+    return render_template("3-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
