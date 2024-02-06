c13c6d0af2c505e98abd34511c42748b8dfa338d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-06 13:33:05 +0300

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+
+if __name__ == "__main__":
+    app.run()
