5ecb9fc80558ec0ac47d0ac3977b2a1ef14e5e26
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-06 10:51:37 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Default route"""
     return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
-     app.run(debug=True)
+    app.run(debug=True)
