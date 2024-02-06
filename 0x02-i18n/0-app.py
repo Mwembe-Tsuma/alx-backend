b033fcbd1115bbbcfe02225fd49cdb59a0837cad
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-06 10:38:07 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+    return render_template(
+            '0-index.html',
+            title='Welcome to Holberton',
+            header='Hello world')
+
 
 if __name__ == "__main__":
     app.run()
