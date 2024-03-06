abbe2471f4c0586c16d9e90c9b517d9d25abb295
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-03-06 10:15:17 +0000

    5-publisher.js

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,34 +1,21 @@
 import redis from 'redis';
-import { promisify } from 'util';
 
 const client = redis.createClient();
 
-client.on('connect', () => {
+client.on('connect', function() {
   console.log('Redis client connected to the server');
 });
 
-client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err}`);
+client.on('error', function(err) {
+  console.log('Redis client not connected to the server: ' + err);
 });
 
 client.subscribe('holberton school channel');
 
-client.on('message', (channel, message) => {
-  console.log(message);
+client.on('message', function(channel, message) {
+  console.log('Message received on channel ' + channel + ': ' + message);
   if (message === 'KILL_SERVER') {
     client.unsubscribe();
     client.quit();
   }
 });
-
-async function publishMessage(message, time) {
-  setTimeout(() => {
-    console.log(`About to send ${message}`);
-    client.publish('holberton school channel', message);
-  }, time);
-}
-
-publishMessage("Holberton Student #1 starts course", 100);
-publishMessage("Holberton Student #2 starts course", 200);
-publishMessage("KILL_SERVER", 300);
-publishMessage("Holberton Student #3 starts course", 400);
